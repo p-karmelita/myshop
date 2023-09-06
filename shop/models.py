e820e48d566bd55b3a6e46389e7a58b439978a58
karpiotr90 <92075179+karpiotr90@users.noreply.github.com>
2023-09-06 19:24:00 +0200

    make catalog views

--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
 
     class Meta:
         ordering = ['name']
@@ -31,6 +35,10 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
 
     class Meta:
         ordering = ['name']
